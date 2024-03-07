26d03c4860624ff298c8b250fe06d168bb12807c
Richard Kadey <richardkadey@gmail.com>
2024-03-07 01:41:00 +0000

    LOGGING

--- visa-approval/logger/__init__.py
+++ visa-approval/logger/__init__.py
@@ -6,6 +6,7 @@ from datetime import datetime
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
 log_dir = "logs"
+
 logs_path = os.path.join(from_root(), log_dir, LOG_FILE)
 
 os.makedirs(log_dir, exist_ok=True)
