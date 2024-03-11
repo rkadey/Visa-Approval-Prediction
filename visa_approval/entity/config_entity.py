4ebce6c83b71344b3af34fc0d6b2466f63ca1388
Richard Kadey <richardkadey@gmail.com>
2024-03-11 18:10:29 +0000

    data validation

--- visa_approval/entity/config_entity.py
+++ visa_approval/entity/config_entity.py
@@ -24,14 +24,12 @@ class DataIngestionConfig:
     collection_name:str = DATA_INGESTION_COLLECTION_NAME
 
 
-
 @dataclass
 class DataValidationConfig:
     data_validation_dir: str = os.path.join(training_pipeline_config.artifact_dir, DATA_VALIDATION_DIR_NAME)
     drift_report_file_path: str = os.path.join(data_validation_dir, DATA_VALIDATION_DRIFT_REPORT_DIR,
                                                DATA_VALIDATION_DRIFT_REPORT_FILE_NAME)
     
-
 
 
 @dataclass
@@ -45,8 +43,6 @@ class DataTransformationConfig:
                                                      DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR,
                                                      PREPROCSSING_OBJECT_FILE_NAME)
     
-
-
 
 @dataclass
 class ModelTrainerConfig:
